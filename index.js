0a616a45ecf8d970677f4a3edcf9d82826a4846d
Florjan96 <florianshiqerukaj@yahoo.com>
2022-06-13 16:33:07 +0200

    Calculating the 2 first taxes with JS

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 let grossSalary=500;
 console.log( "The gross Salary =" + grossSalary + "$");
+
+
+let employeePension=0.05;
+
+console.log("The employee pension is"+ "" +"5%");
+
+let taxableAmount= grossSalary *employeePension;
+console.log("The taxable amount is =" + taxableAmount + "$");
+
+
+
+let myTax=0.1;
+
+let secondTax=grossSalary*myTax;
+
+console.log("My second tax that depends on my gross salary is =" + secondTax + "$" );
